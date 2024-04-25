dde548822e5009af6f1c418ccf95e868e2588eb1
ronak904 <ronakmandal904@gmail.com>
2024-04-25 11:02:16 -0400

    update

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="ronakmandal904@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+console.log(accountId);
+
+accountEmail="hc@hc.com"
+accountPassword= "21212121"
+accountCity= "jaipur";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
